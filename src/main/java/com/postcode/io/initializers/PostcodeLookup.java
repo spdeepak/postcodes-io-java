a4d1e035266f32cbae0910df319bacb6ef88343c
Deepak Prabhakar <speedpak1991@gmail.com>
2016-08-14 10:24:39 +0100

    Made PostcodeLookup JSON fetchable for single and multiple postcode.

--- src/main/java/com/postcode/io/initializers/PostcodeLookup.java
+++ src/main/java/com/postcode/io/initializers/PostcodeLookup.java
@@ -3,6 +3,13 @@ package com.postcode.io.initializers;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.codehaus.plexus.util.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.postcode.io.PostCode;
+import com.postcode.io.json.JsonFetcher;
+
 /**
  * Lookup a postcode
  * 
@@ -15,11 +22,14 @@ public class PostcodeLookup {
 
     private String postcode;
 
+    private String[] postcodes;
+
     private String url;
 
-    private PostcodeLookup(URL url, String postcode) {
+    private PostcodeLookup(URL url, String postcode, String[] postcodes) {
         this.url = url.toString();
         this.postcode = postcode;
+        this.postcodes = postcodes;
     }
 
     public String getPostcode() {
@@ -40,16 +50,64 @@ public class PostcodeLookup {
 
         private String postcode;
 
+        private String[] postcodes;
+
+        private JSONObject json;
+
         public Builder() {
         }
 
+        /**
+         * Use this to create {@link PostCode}<br/>
+         * Pass this to {@link PostCode#generate}
+         * 
+         * @return
+         * @throws MalformedURLException
+         */
         public PostcodeLookup build() throws MalformedURLException {
-            return new PostcodeLookup(new URL(LOOKUP_URL.toString().concat(postcode)), postcode);
+            return new PostcodeLookup(new URL(LOOKUP_URL.toString().concat(postcode)), postcode, postcodes);
         }
 
+        public JSONObject asJson() throws Exception {
+            if (!StringUtils.isEmpty(postcode)) {
+                return JsonFetcher.urlToJson(new URL(LOOKUP_URL.toString().concat(postcode)));
+            } else if (json != null) {
+                return JsonFetcher.postURLToJson(new URL(LOOKUP_URL.toString()), json);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        /**
+         * Use if information required is for only one {@link PostCode}
+         * 
+         * @param postcode
+         * @return
+         */
         public Builder postcode(String postcode) {
             this.postcode = postcode;
             return this;
+        }
+
+        /**
+         * Use if information required is for multiple {@link PostCode}
+         * 
+         * @param postcodes
+         * @return
+         */
+        public Builder postcodes(String[] postcodes) {
+            this.postcodes = postcodes;
+            createJsonPostcodes(postcodes);
+            return this;
+        }
+
+        private void createJsonPostcodes(String[] postcodes) {
+            JSONObject json = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            for (String string : postcodes) {
+                jsonArray.put(string);
+            }
+            this.json = json.put("postcodes", jsonArray);
         }
     }
 
--- src/test/java/com/postcode/io/initializers/PostcodeLookupTest.java
+++ src/test/java/com/postcode/io/initializers/PostcodeLookupTest.java
@@ -2,9 +2,15 @@ package com.postcode.io.initializers;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.net.MalformedURLException;
 
+import org.json.JSONException;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import com.postcode.io.json.JsonFetcher;
 
 /**
  * @author Deepak
@@ -17,6 +23,25 @@ public class PostcodeLookupTest {
         PostcodeLookup postcodeLookup = new PostcodeLookup.Builder().postcode("bs347np").build();
         assertEquals(postcodeLookup.getPostcode(), "bs347np");
         assertEquals(postcodeLookup.getUrl(), "http://api.postcodes.io/postcodes/bs347np");
+    }
+
+    @Test
+    public void testPostCodeLookupAsJsonForSinglePostcode() throws JSONException, MalformedURLException, Exception {
+        JSONAssert.assertEquals(
+                JsonFetcher.urlToJson(
+                        new File(System.getProperty("user.dir").concat("/src/test/resources/postcodeLookup.json"))
+                                .toURI().toURL()),
+                new PostcodeLookup.Builder().postcode("bs347np").asJson(), JSONCompareMode.STRICT);
+
+    }
+
+    @Test
+    public void testPostCodeLookupAsJsonForMultiplePostcodes() throws JSONException, MalformedURLException, Exception {
+        JSONAssert.assertEquals(JsonFetcher.urlToJson(
+                new File(System.getProperty("user.dir").concat("/src/test/resources/postcodeLookupViaPOST.json"))
+                        .toURI().toURL()),
+                new PostcodeLookup.Builder().postcodes(new String[] { "OX49 5NU", "M32 0JG", "NE30 1DP" }).asJson(),
+                JSONCompareMode.LENIENT);
     }
 
 }
